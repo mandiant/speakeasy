36fb56dea253efa340382b4f2f55be787830a9e1
Matt Williams <matt.williams31@gmail.com>
2020-10-05 11:55:50 -0400

    Added COM APIs

--- speakeasy/winenv/api/usermode/ole32.py
+++ speakeasy/winenv/api/usermode/ole32.py
@@ -2,6 +2,7 @@
 
 import uuid
 
+import speakeasy.winenv.defs.windows.com as com
 import speakeasy.winenv.defs.windows.windows as windefs
 
 from .. import api
@@ -31,6 +32,91 @@ class Ole32(api.ApiHandler):
         """
 
         rv = windefs.S_OK
+
+        return rv
+
+    @apihook('CoInitialize', argc=1)
+    def CoInitialize(self, emu, argv, ctx={}):
+        """
+        HRESULT CoInitialize(
+          LPVOID pvReserved
+        );
+        """
+
+        rv = windefs.S_OK
+
+        return rv
+
+    @apihook('CoInitializeEx', argc=2)
+    def CoInitializeEx(self, emu, argv, ctx={}):
+        """
+        HRESULT CoInitializeEx(
+          LPVOID pvReserved,
+          DWORD  dwCoInit
+        );
+        """
+
+        rv = windefs.S_OK
+
+        return rv
+
+    @apihook('CoUninitialize', argc=0)
+    def CoUninitialize(self, emu, argv, ctx={}):
+        """
+        void CoUninitialize();
+        """
+
+    @apihook('CoInitializeSecurity', argc=9)
+    def CoInitializeSecurity(self, emu, argv, ctx={}):
+        """
+        HRESULT CoInitializeSecurity(
+          PSECURITY_DESCRIPTOR        pSecDesc,
+          LONG                        cAuthSvc,
+          SOLE_AUTHENTICATION_SERVICE *asAuthSvc,
+          void                        *pReserved1,
+          DWORD                       dwAuthnLevel,
+          DWORD                       dwImpLevel,
+          void                        *pAuthList,
+          DWORD                       dwCapabilities,
+          void                        *pReserved3
+        );
+        """
+
+        rv = windefs.S_OK
+
+        authn_level = com.get_define(argv[4])
+        if authn_level:
+            argv[4] = authn_level
+
+        imp_level = com.get_define(argv[5])
+        if imp_level:
+            argv[5] = imp_level
+
+        return rv
+
+    @apihook('CoCreateInstance', argc=5)
+    def CoCreateInstance(self, emu, argv, ctx={}):
+        """
+        HRESULT CoCreateInstance(
+          REFCLSID  rclsid,
+          LPUNKNOWN pUnkOuter,
+          DWORD     dwClsContext,
+          REFIID    riid,
+          LPVOID    *ppv
+        );
+        """
+        rclsid, pUnkOuter, dwClsContext, riid, ppv = argv
+        rv = windefs.S_OK
+
+        clsid_guid = self.mem_read(rclsid, self.sizeof(windefs.GUID()))
+        clsid_u = uuid.UUID(bytes_le=clsid_guid)
+        clsid_u = ('{%s}' % (clsid_u)).upper()
+        argv[0] = clsid_u
+
+        riid_guid = self.mem_read(riid, self.sizeof(windefs.GUID()))
+        riid_u = uuid.UUID(bytes_le=riid_guid)
+        riid_u = ('{%s}' % (riid_u)).upper()
+        argv[3] = riid_u
 
         return rv
 
--- speakeasy/winenv/defs/windows/com.py
+++ speakeasy/winenv/defs/windows/com.py
@@ -2,6 +2,20 @@
 
 from speakeasy.struct import EmuStruct, Ptr
 
+RPC_C_AUTHN_LEVEL_DEFAULT = 0
+RPC_C_AUTHN_LEVEL_NONE = 1
+RPC_C_AUTHN_LEVEL_CONNECT = 2
+RPC_C_AUTHN_LEVEL_CALL = 3
+RPC_C_AUTHN_LEVEL_PKT = 4
+RPC_C_AUTHN_LEVEL_PKT_INTEGRITY = 5
+RPC_C_AUTHN_LEVEL_PKT_PRIVACY = 6
+
+RPC_C_IMP_LEVEL_DEFAULT = 0
+RPC_C_IMP_LEVEL_ANONYMOUS = 1
+RPC_C_IMP_LEVEL_IDENTIFY = 2
+RPC_C_IMP_LEVEL_IMPERSONATE = 3
+RPC_C_IMP_LEVEL_DELEGATE = 4
+
 
 class ComInterface(object):
     def __init__(self, iface, name, ptr_size):
@@ -32,3 +46,22 @@ class IMalloc(EmuStruct):
 
 IFACE_TYPES = {'IUnknown': IUnknown,
                'IMalloc':  IMalloc}
+
+
+def get_define(define, prefix=''):
+    for k, v in globals().items():
+        if not isinstance(v, int) or v != define:
+            continue
+        if prefix:
+            if k.startswith(prefix):
+                return k
+        else:
+            return k
+
+
+def get_rpc_authlevel(define):
+    return get_define(define, prefix='RPC_C_AUTHN_LEVEL_')
+
+
+def get_rcp_implevel(define):
+    return get_define(define, prefix='RPC_C_IMP_LEVEL_')
