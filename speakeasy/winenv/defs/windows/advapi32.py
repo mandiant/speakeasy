e8c567aa1bc6f8c537dabc2e901372e9ce61bdc3
Matt Williams <13837569+mwilliams31@users.noreply.github.com>
2020-12-16 13:58:48 -0500

    Added advapi32.EnumServicesStatus

--- speakeasy/winenv/defs/windows/advapi32.py
+++ speakeasy/winenv/defs/windows/advapi32.py
@@ -4,9 +4,24 @@ from speakeasy.struct import EmuStruct, Ptr
 
 NTE_BAD_ALGID = 0x80090008
 
+SERVICE_WIN32 = 0x30
+
+SERVICE_ACTIVE = 0x1
+
 
 class SERVICE_TABLE_ENTRY(EmuStruct):
     def __init__(self, ptr_size):
         super().__init__(ptr_size)
         self.lpServiceName = Ptr
         self.lpServiceProc = Ptr
+
+
+def get_define_int(define, prefix=''):
+    for k, v in globals().items():
+        if not isinstance(v, int) or v != define:
+            continue
+        if prefix:
+            if k.startswith(prefix):
+                return k
+        else:
+            return k
