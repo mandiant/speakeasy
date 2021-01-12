069cbf93babdf981db99db1759f76bf5167d1c96
Matt Williams <13837569+mwilliams31@users.noreply.github.com>
2021-01-12 16:03:14 -0500

    NetWkstaGetInfo and NetApiBufferFree

--- speakeasy/winenv/api/usermode/NETAPI32.py
+++ speakeasy/winenv/api/usermode/NETAPI32.py
@@ -1,20 +1,20 @@
-# Copyright (C) 2020 FireEye, Inc. All Rights Reserved.
+# Copyright (C) 2021 FireEye, Inc. All Rights Reserved.
 
 import speakeasy.winenv.defs.windows.kernel32 as k32types
+import speakeasy.winenv.defs.windows.netapi32 as netapi32defs
 import speakeasy.winenv.arch as _arch
+
 from .. import api
 
-PAGE_SIZE = 0x1000
 
-
-class net32api(api.ApiHandler):
+class NetApi32(api.ApiHandler):
     name = 'NETAPI32'
     apihook = api.ApiHandler.apihook
     impdata = api.ApiHandler.impdata
 
     def __init__(self, emu):
-        super(net32api, self).__init__(emu)
-        super(net32api, self).__get_hook_attrs__(self)
+        super(NetApi32, self).__init__(emu)
+        super(NetApi32, self).__get_hook_attrs__(self)
 
     @apihook('NetGetJoinInformation', argc=3, conv=_arch.CALL_CONV_STDCALL)
     def NetGetJoinInformation(self, emu, argv, ctx={}):
@@ -41,4 +41,73 @@ class net32api(api.ApiHandler):
         lpNameBuffer = name_bytes
         BufferType = k32types.NetSetupWorkgroupName
 
-        return 0  # NERR_Success
+        return netapi32defs.NERR_Success
+
+    @apihook('NetWkstaGetInfo', argc=3)
+    def NetWkstaGetInfo(self, emu, argv, ctx={}):
+        """
+        NET_API_STATUS NET_API_FUNCTION NetWkstaGetInfo(
+          LMSTR  servername,
+          DWORD  level,
+          LPBYTE *bufptr
+        );
+        """
+        servername, level, bufptr = argv
+
+        if level not in [100, 101, 102]:
+            return netapi32defs.ERROR_INVALID_LEVEL
+
+        if level == 100:
+            wki = netapi32defs.WKSTA_INFO_100(emu.get_ptr_size())
+        elif level == 101:
+            wki = netapi32defs.WKSTA_INFO_101(emu.get_ptr_size())
+
+            # Using empty string
+            lanroot_ptr = self.mem_alloc(2)
+            self.mem_write(lanroot_ptr, b'\x00\x00')
+            wki.wki_lanroot = lanroot_ptr
+        else:
+            wki = netapi32defs.WKSTA_INFO_102(emu.get_ptr_size())
+
+            # Using empty string
+            lanroot_ptr = self.mem_alloc(2)
+            self.mem_write(lanroot_ptr, b'\x00\x00')
+            wki.wki_lanroot = lanroot_ptr
+
+            wki.wki_logged_on_users = 2
+
+        wki_addr = self.mem_alloc(wki.sizeof())
+        self.mem_cast(wki, wki_addr)
+
+        platform_id = 500  # PLATFORM_ID_NT
+        wki.wki_platform_id = platform_id
+
+        hostname = emu.get_hostname()
+        computername_ptr = self.mem_alloc(2 + (len(hostname) * 2))
+        # Assuming Unicode; "This string is Unicode if _WIN32_WINNT or
+        # FORCE_UNICODE are defined."
+        self.write_mem_string(hostname, computername_ptr, width=2)
+        wki.wki_computername = computername_ptr
+
+        domain = emu.get_domain()
+        langroup_ptr = self.mem_alloc(2 + (len(domain) * 2))
+        self.write_mem_string(domain, langroup_ptr, width=2)
+        wki.wki_langroup = langroup_ptr
+
+        osver = emu.get_os_version()
+        wki.wki_ver_major = osver['major']
+        wki.wki_ver_minor = osver['minor']
+
+        self.mem_write(wki_addr, wki.get_bytes())
+        self.mem_write(bufptr, wki_addr.to_bytes(emu.get_ptr_size(), 'little'))
+
+        return netapi32defs.NERR_Success
+
+    @apihook('NetApiBufferFree', argc=1)
+    def NetApiBufferFree(self, emu, argv, ctx={}):
+        """
+        NET_API_STATUS NET_API_FUNCTION NetApiBufferFree(
+          _Frees_ptr_opt_ LPVOID Buffer
+        );
+        """
+        return netapi32defs.NERR_Success
---
+++ speakeasy/winenv/defs/windows/netapi32.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2021 FireEye, Inc. All Rights Reserved.
+
+from speakeasy.struct import EmuStruct, Ptr
+import ctypes as ct
+
+NERR_Success = 0
+ERROR_INVALID_LEVEL = 0x7C
+
+
+class WKSTA_INFO_100(EmuStruct):
+    def __init__(self, ptr_size):
+        super().__init__(ptr_size)
+        self.wki_platform_id = Ptr
+        self.wki_computername = Ptr
+        self.wki_langroup = Ptr
+        self.wki_ver_major = ct.c_uint32
+        self.wki_ver_minor = ct.c_uint32
+
+
+class WKSTA_INFO_101(EmuStruct):
+    def __init__(self, ptr_size):
+        super().__init__(ptr_size)
+        self.wki_platform_id = Ptr
+        self.wki_computername = Ptr
+        self.wki_langroup = Ptr
+        self.wki_ver_major = ct.c_uint32
+        self.wki_ver_minor = ct.c_uint32
+        self.wki_lanroot = Ptr
+
+
+class WKSTA_INFO_102(EmuStruct):
+    def __init__(self, ptr_size):
+        super().__init__(ptr_size)
+        self.wki_platform_id = Ptr
+        self.wki_computername = Ptr
+        self.wki_langroup = Ptr
+        self.wki_ver_major = ct.c_uint32
+        self.wki_ver_minor = ct.c_uint32
+        self.wki_lanroot = Ptr
+        self.wki_logged_on_users = Ptr
