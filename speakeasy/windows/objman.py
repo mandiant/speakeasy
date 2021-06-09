21f10ecbfde97af7d2c221ba54f167d2ecbe8724
cecio <b38911@gmail.com>
2021-06-10 00:35:10 +0200

    Fix for issue #139 ldr_entries initialization moved into __init__ of Process object

--- speakeasy/windows/objman.py
+++ speakeasy/windows/objman.py
@@ -501,12 +501,12 @@ class Process(KernelObject):
     """
     An EPROCESS object used by the Windows kernel to represent a process
     """
-    ldr_entries = []
 
     def __init__(self, emu, pe=None, user_modules=[],
                  name='', path='', cmdline='', base=0, session=0):
         super(Process, self).__init__(emu=emu)
         # TODO: For now just allocate a blank opaque struct for an EPROCESS
+        ldr_entries = []
         self.object = self.nt_types.EPROCESS(emu.get_ptr_size())
         self.address = emu.mem_map(self.sizeof(), tag=self.get_mem_tag(), perms=1, base=0xe0000000)
         self.name = name
