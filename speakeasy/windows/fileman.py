3ac8a1d5bc6d6cd878eab3a384cdf713c7557912
0ssigeno <simone.berni2@studio.unibo.it>
2021-08-02 16:37:03 +0200

    General fixes

--- speakeasy/windows/fileman.py
+++ speakeasy/windows/fileman.py
@@ -212,7 +212,7 @@ class FileManager(object):
 
         if cmdline is None:
             cmdline = ""
-        self.emulated_binname = shlex.split(cmdline if cmdline else emu.command_line)[0]
+        self.emulated_binname = shlex.split(cmdline)[0] if cmdline else emu.get_argv()[0]
 
         # First file in this list seems to always be the module itself
         self.files = []
