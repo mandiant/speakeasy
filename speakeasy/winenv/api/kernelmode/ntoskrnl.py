mandiant/speakeasy

commit 0dedb7f0dfb91eb54e1e5932259c92bd0ae63b6b

Support for unhandled exception filters, further API adds

speakeasy/winenv/api/kernelmode/ntoskrnl.py
@@ -8,6 +8,7 @@ import lznt1
 import speakeasy.winenv.arch as _arch
 import speakeasy.winenv.defs.nt.ddk as ddk
 import speakeasy.winenv.defs.registry.reg as regdefs
+import speakeasy.winenv.defs.windows.windows as windefs
 import speakeasy.winenv.defs.nt.ntoskrnl as ntos
 from speakeasy.errors import ApiEmuError
 from speakeasy.winenv.api import api
@@ -1365,6 +1366,9 @@ class Ntoskrnl(api.ApiHandler):
         size = int.from_bytes(self.mem_read(RegionSize,
                                             self.get_ptr_size()), 'little')
 
+        base = self.mem_read(BaseAddress, emu.get_ptr_size())
+        base = int.from_bytes(base, 'little')
+        argv[1] = '0x%x->0x%x' % (BaseAddress, base)
         base = self.mem_alloc(size, tag='api.virtalloc.%s' % obj.image, process=obj)
 
         emu._set_dyn_code_hook(base, size)
@@ -2938,6 +2942,47 @@ class Ntoskrnl(api.ApiHandler):
         block = self.heap_alloc(size, heap='RtlAllocateHeap')
 
         return block
+
+    @apihook('ZwGetContextThread', argc=2)
+    def ZwGetContextThread(self, emu, argv, ctx={}):
+        '''
+        BOOL ZwGetContextThread(
+            HANDLE    hThread,
+            LPCONTEXT lpContext
+        );
+        '''
+        hThread, lpContext = argv
+
+        obj = self.get_object_from_handle(hThread)
+        if not obj:
+            return False
+
+        context = obj.get_context()
+
+        self.mem_write(lpContext, context.get_bytes())
+
+        return True
+
+    @apihook('ZwSetContextThread', argc=2)
+    def ZwSetContextThread(self, emu, argv, ctx={}):
+        '''
+        BOOL ZwSetContextThread(
+            HANDLE    hThread,
+            LPCONTEXT lpContext
+        );
+        '''
+        hThread, lpContext = argv
+
+        obj = self.get_object_from_handle(hThread)
+        if not obj:
+            return False
+
+        context = windefs.CONTEXT(emu.get_ptr_size())
+        if lpContext:
+            _context = self.mem_cast(context, lpContext)
+            obj.set_context(_context)
+
+        return True
 
     @apihook('RtlFreeHeap', argc=3)
     def RtlFreeHeap(self, emu, argv, ctx={}):

speakeasy/windows/common.py
@@ -128,7 +128,6 @@ class PeFile(pefile.PE):
         max_tls_callbacks = 100
         callbacks = []
         if hasattr(self, 'DIRECTORY_ENTRY_TLS'):
-            print('got tls')
             rva = (self.DIRECTORY_ENTRY_TLS.struct.AddressOfCallBacks -
                    self.OPTIONAL_HEADER.ImageBase)
 

speakeasy/windows/win32.py
@@ -231,11 +231,12 @@ class Win32Emulator(WindowsEmulator):
         tls = module.get_tls_callbacks()
         for i, cb_addr in enumerate(tls):
             base = module.get_base()
-            run = Run()
-            run.start_addr = cb_addr
-            run.type = 'tls_callback_%d' % (i)
-            run.args = [base, DLL_PROCESS_ATTACH, 0]
-            self.add_run(run)
+            if base < cb_addr < base + module.get_image_size():
+                run = Run()
+                run.start_addr = cb_addr
+                run.type = 'tls_callback_%d' % (i)
+                run.args = [base, DLL_PROCESS_ATTACH, 0]
+                self.add_run(run)
 
         # Queue up the module's main entry point
         ep = module.base + module.ep
@@ -458,6 +459,12 @@ class Win32Emulator(WindowsEmulator):
         peb.object.ImageBaseAddress = proc.base
         peb.write_back()
         return peb
+
+    def set_unhandled_exception_handler(self, handler_addr):
+        """
+        Establish a handler for unhandled exceptions that occur during emulation
+        """
+        self.unhandled_exception_filter = handler_addr
 
     def setup(self, stack_commit=0):
 

speakeasy/windows/winemu.py
@@ -83,6 +83,7 @@ class WindowsEmulator(BinaryEmulator):
         self.curr_thread = None
         self.curr_exception_code = 0
         self.prev_pc = 0
+        self.unhandled_exception_filter = 0
 
         self.fs_addr = 0
         self.gs_addr = 0
@@ -922,8 +923,9 @@ class WindowsEmulator(BinaryEmulator):
                 self._unset_emu_hooks()
                 return True
 
+        # Are there any SEH handlers registered?
         if self.dispatch_handlers:
-            rv = self.dispatch_seh(ddk.STATUS_ACCESS_VIOLATION)
+            rv = self.dispatch_seh(ddk.STATUS_ACCESS_VIOLATION, address)
             if rv:
                 return True
 
@@ -940,9 +942,10 @@ class WindowsEmulator(BinaryEmulator):
         """
         Collect emulator state information in the event of an error
         """
+        run = self.get_current_run()
         pc = self.get_pc()
         error = {}
-        self.log_error('Caught error: %s' % (desc))
+        self.log_error('%s: Caught error: %s' % (run.type, desc))
         error['type'] = desc
         error['pc'] = hex(pc)
         error['address'] = hex(address)
@@ -1331,7 +1334,7 @@ class WindowsEmulator(BinaryEmulator):
             return True
 
         if self.dispatch_handlers:
-            rv = self.dispatch_seh(ddk.STATUS_ACCESS_VIOLATION)
+            rv = self.dispatch_seh(ddk.STATUS_ACCESS_VIOLATION, address)
             if rv:
                 return True
         fakeout = address & 0xFFFFFFFFFFFFF000
@@ -1371,7 +1374,7 @@ class WindowsEmulator(BinaryEmulator):
         Called when non-writable address is written to
         """
         if self.dispatch_handlers:
-            rv = self.dispatch_seh(ddk.STATUS_ACCESS_VIOLATION)
+            rv = self.dispatch_seh(ddk.STATUS_ACCESS_VIOLATION, address)
             if rv:
                 return True
 
@@ -1910,9 +1913,40 @@ class WindowsEmulator(BinaryEmulator):
 
         self.run_complete = True
 
-    def dispatch_seh(self, except_code):
+    def dispatch_seh(self, except_code, faulting_address=None):
+        rv = False
         if self.get_arch() == _arch.ARCH_X86:
-            return self._dispatch_seh_x86(except_code)
+            rv = self._dispatch_seh_x86(except_code)
+        if not rv and self.unhandled_exception_filter:
+            # Create the _EXCEPTION_RECORD
+            record = self.wintypes.EXCEPTION_RECORD(self.get_ptr_size())
+            record.ExceptionCode = except_code
+            record.ExceptionFlags = 0
+            record.ExceptionAddress = self.get_pc()
+            record.NumberParameters = 0
+
+            exp_ptrs = self.wintypes.EXCEPTION_POINTERS(self.get_ptr_size())
+            p_exp_ptrs = self.mem_map(exp_ptrs.sizeof(), tag='emu.struct.EXCEPTION_POINTERS')
+            prec = self.mem_map(record.sizeof(), tag='emu.struct.EXCEPTION_RECORD')
+            pctx = self.mem_map(record.sizeof(), tag='emu.struct.EXCEPTION_CONTEXT')
+
+            exp_ptrs.ExceptionRecord = prec
+            exp_ptrs.ContextRecord = pctx
+
+            self.mem_write(p_exp_ptrs, exp_ptrs.get_bytes())
+            self.mem_write(prec, record.get_bytes())
+
+            sp = self.get_stack_ptr()
+            args = [p_exp_ptrs]
+            self.set_func_args(sp, winemu.EMU_RETURN_ADDR, *args)
+            self.set_pc(self.unhandled_exception_filter)
+            self.unhandled_exception_filter = 0
+            if faulting_address:
+                fakeout = faulting_address & 0xFFFFFFFFFFFFF000
+                self.mem_map(self.page_size, base=fakeout)
+                self.tmp_maps.append((fakeout, self.page_size))
+            rv = True
+        return rv
 
     def continue_seh(self):
         if self.get_arch() == _arch.ARCH_X86:

speakeasy/winenv/api/usermode/kernel32.py
@@ -473,6 +473,30 @@ class Kernel32(api.ApiHandler):
 
         return hmod
 
+    @apihook('CreateProcessInternal', argc=12)
+    def CreateProcessInternal(self, emu, argv, ctx={}):
+        '''
+        BOOL CreateProcessInternal(
+          PVOID Reserved1,
+          LPTSTR                lpApplicationName,
+          LPTSTR                lpCommandLine,
+          LPSECURITY_ATTRIBUTES lpProcessAttributes,
+          LPSECURITY_ATTRIBUTES lpThreadAttributes,
+          BOOL                  bInheritHandles,
+          DWORD                 dwCreationFlags,
+          LPVOID                lpEnvironment,
+          LPTSTR                lpCurrentDirectory,
+          LPSTARTUPINFO         lpStartupInfo,
+          LPPROCESS_INFORMATION lpProcessInformation,
+          PVOID Reserved2
+        );
+        '''
+        # Args are the same as CreateProcess except for argv[0] and argv[-1]
+        _argv = argv[1:-1]
+        rv = self.CreateProcess(emu, _argv, ctx)
+        argv[1:-1] = _argv
+        return rv
+
     @apihook('CreateProcess', argc=10)
     def CreateProcess(self, emu, argv, ctx={}):
         '''BOOL CreateProcess(
@@ -2962,6 +2986,9 @@ class Kernel32(api.ApiHandler):
           LPTOP_LEVEL_EXCEPTION_FILTER lpTopLevelExceptionFilter
         );
         '''
+        lpTopLevelExceptionFilter, = argv
+
+        emu.set_unhandled_exception_handler(lpTopLevelExceptionFilter)
 
         return 0
 

speakeasy/winenv/api/usermode/ntdll.py
@@ -41,6 +41,11 @@ class Ntdll(api.ApiHandler):
 
         return emu.get_last_error()
 
+    @apihook('RtlFlushSecureMemoryCache', argc=2)
+    def RtlFlushSecureMemoryCache(self, emu, argv, ctx={}):
+        '''DWORD RtlFlushSecureMemoryCache(PVOID arg0, PVOID arg1);'''
+        return True
+
     @apihook('RtlAddVectoredExceptionHandler', argc=2)
     def RtlAddVectoredExceptionHandler(self, emu, argv, ctx={}):
         '''

commit 046d0fb8eff3d20e18581cf4c133fee8d220e073

Remove debug output for argv[1]
Remove debug output for argv[1] in ntoskrnl.py introduced in commit 0dedb7f0dfb91eb54e1e5932259c92bd0ae63b6b

speakeasy/winenv/api/kernelmode/ntoskrnl.py
@@ -1437,7 +1437,6 @@ class Ntoskrnl(api.ApiHandler):
 
         base = self.mem_read(BaseAddress, emu.get_ptr_size())
         base = int.from_bytes(base, 'little')
-        argv[1] = '0x%x->0x%x' % (BaseAddress, base)
         base = self.mem_alloc(size, tag='api.virtalloc.%s' % obj.image, process=obj)
 
         emu._set_dyn_code_hook(base, size)