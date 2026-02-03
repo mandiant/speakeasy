62d8e37221a373b01fe690232411dd203c948f56
Willi Ballenthin <wballenthin@hex-rays.com>
2026-02-03 13:22:46 +0100

    tests: migrate to pytest from unittest

--- tests/test_GetProcAddress.py
+++ tests/test_GetProcAddress.py
@@ -1,32 +1,19 @@
-# test call to GetProcAddress by using a not exported function
-
-import unittest
-
-import util
+# Copyright (C) 2020 FireEye, Inc. All Rights Reserved.
 
 
-class TestDllEmulation(unittest.TestCase):
+def get_api_calls(ep, api_name):
+    return [api for api in ep['apis'] if api['api_name'] == api_name]
 
-    def setUp(self):
-        self.config = util.get_config()
-        self.report = None
 
-    def _get_api_calls(self, ep, api_name):
-        return [api for api in ep['apis'] if api['api_name'] == api_name]
+def test_GetProcAddress_on_not_existing_function_fails(config, load_test_bin, run_test):
+    data = load_test_bin('GetProcAddress.exe.xz')
+    report = run_test(config, data)
+    eps = report['entry_points']
 
-    def test_GetProcAddress_on_not_existing_function_fails(self):
-        fpath = 'GetProcAddress.exe.xz'
-        data = util.get_test_bin_data(fpath)
-        self.report = util.run_test(self.config, data)
-        eps = self.report['entry_points']
+    get_proc_addr = get_api_calls(eps[0], 'KERNEL32.GetProcAddress')
 
-        get_proc_addr = self._get_api_calls(eps[0], 'KERNEL32.GetProcAddress')
+    assert get_proc_addr[2]['args'][1] == 'AreFileApisANSI'
+    assert get_proc_addr[2]['ret_val'] != '0x0'
 
-        self.assertEqual(get_proc_addr[2]['args'][1], 'AreFileApisANSI')
-        self.assertNotEqual(get_proc_addr[2]['ret_val'], '0x0')
-
-        self.assertEqual(get_proc_addr[3]['args'][1], 'ThisFunctionIsNotExportedByKernel32')
-        self.assertEqual(get_proc_addr[3]['ret_val'], '0x0')
-
-if __name__ == '__main__':
-    unittest.main()
+    assert get_proc_addr[3]['args'][1] == 'ThisFunctionIsNotExportedByKernel32'
+    assert get_proc_addr[3]['ret_val'] == '0x0'
