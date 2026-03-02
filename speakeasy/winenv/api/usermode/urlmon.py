d7c726ae92f00da3ded4f47f3909a354918a6585
Willi Ballenthin <willi.ballenthin@gmail.com>
2026-03-02 08:22:07 +0000

    hook UrlDownloadToCacheFile

--- speakeasy/winenv/api/usermode/urlmon.py
+++ speakeasy/winenv/api/usermode/urlmon.py
@@ -42,14 +42,55 @@ class Urlmon(api.ApiHandler):
         if szURL:
             url = self.read_mem_string(szURL, cw)
             argv[1] = url
-            url = urlparse(url)
-            if url.netloc:
-                self.record_dns_event(url.netloc)
+            parsed = urlparse(url)
+            if parsed.netloc:
+                self.record_dns_event(parsed.netloc)
 
         if szFileName:
             name = self.read_mem_string(szFileName, cw)
             argv[2] = name
             self.record_file_access_event(name, FILE_CREATE)
             self.record_file_access_event(name, FILE_WRITE)
+
+        return rv
+
+    @apihook("URLDownloadToCacheFile", argc=6)
+    def URLDownloadToCacheFile(self, emu, argv, ctx={}):
+        """
+        HRESULT URLDownloadToCacheFileA(
+          LPUNKNOWN            pCaller,
+          LPCSTR               szURL,
+          LPSTR                szFileName,
+          DWORD                cchFileName,
+          DWORD                dwReserved,
+          LPBINDSTATUSCALLBACK lpfnCB
+        );
+        """
+        pCaller, szURL, szFileName, cchFileName, dwReserved, lpfnCB = argv
+        rv = windefs.ERROR_SUCCESS
+        cw = self.get_char_width(ctx)
+
+        cache_name = "C:\\Windows\\Temp\\urlcache.bin"
+
+        if szURL:
+            url = self.read_mem_string(szURL, cw)
+            argv[1] = url
+            parsed = urlparse(url)
+            if parsed.netloc:
+                self.record_dns_event(parsed.netloc)
+            tail = parsed.path.rsplit("/", 1)[-1]
+            if tail:
+                cache_name = f"C:\\Windows\\Temp\\{tail}"
+
+        if szFileName:
+            required = len(cache_name) + 1
+            argv[2] = cache_name
+            argv[3] = cchFileName
+            if cchFileName >= required:
+                self.write_mem_string(cache_name, szFileName, cw)
+                self.record_file_access_event(cache_name, FILE_CREATE)
+                self.record_file_access_event(cache_name, FILE_WRITE)
+            else:
+                rv = windefs.ERROR_INSUFFICIENT_BUFFER
 
         return rv
