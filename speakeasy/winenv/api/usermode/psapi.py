a836b0cc80fbd62fc95c49d4497a146c1add1342
Willi Ballenthin <willi.ballenthin@gmail.com>
2026-03-02 12:38:51 +0000

    add psapi hooks

---
+++ speakeasy/winenv/api/usermode/psapi.py
@@ -0,0 +1,115 @@
+# Copyright (C) 2026 Mandiant, Inc. All Rights Reserved.
+
+import ntpath
+
+from .. import api
+
+
+class Psapi(api.ApiHandler):
+    name = "psapi"
+    apihook = api.ApiHandler.apihook
+    impdata = api.ApiHandler.impdata
+
+    def __init__(self, emu):
+
+        super().__init__(emu)
+        self.funcs = {}
+        self.data = {}
+        super().__get_hook_attrs__(self)
+
+    def _get_process_module_bases(self, proc):
+        proc_module = proc.get_module()
+        if proc_module:
+            return [proc_module.get_base()]
+
+        process_path = proc.get_process_path() or ""
+        process_name = ntpath.basename(process_path)
+        module_name = ntpath.splitext(process_name)[0]
+        if module_name:
+            mod = self.emu.get_mod_by_name(module_name)
+            if mod:
+                return [mod.get_base()]
+
+        return []
+
+    def _get_module_base_name(self, proc, hModule):
+        if hModule:
+            mod = self.emu.get_mod_from_addr(hModule)
+            if mod:
+                return ntpath.basename(mod.get_emu_path())
+
+        return ntpath.basename(proc.get_process_path() or "")
+
+    @apihook("EnumProcesses", argc=3)
+    def EnumProcesses(self, emu, argv, ctx={}):
+        lpidProcess, cb, lpcbNeeded = argv
+        processes = emu.get_processes()
+
+        if lpcbNeeded:
+            self.mem_write(lpcbNeeded, (len(processes) * 4).to_bytes(4, "little"))
+
+        if not lpidProcess or cb < 4:
+            return 1
+
+        count = min(cb // 4, len(processes))
+        cursor = lpidProcess
+        for process in processes[:count]:
+            pid = process.get_pid() or 0
+            self.mem_write(cursor, int(pid).to_bytes(4, "little"))
+            cursor += 4
+
+        return 1
+
+    @apihook("EnumProcessModules", argc=4)
+    def EnumProcessModules(self, emu, argv, ctx={}):
+        hProcess, lphModule, cb, lpcbNeeded = argv
+        proc = self.get_object_from_handle(hProcess)
+        if not proc:
+            return 0
+
+        module_bases = self._get_process_module_bases(proc)
+        ptr_size = self.get_ptr_size()
+
+        if lpcbNeeded:
+            self.mem_write(lpcbNeeded, (len(module_bases) * ptr_size).to_bytes(4, "little"))
+
+        if not lphModule or cb < ptr_size:
+            return 1
+
+        count = min(cb // ptr_size, len(module_bases))
+        cursor = lphModule
+        for module_base in module_bases[:count]:
+            self.mem_write(cursor, int(module_base).to_bytes(ptr_size, "little"))
+            cursor += ptr_size
+
+        return 1
+
+    @apihook("GetModuleBaseName", argc=4)
+    @apihook("GetModuleBaseNameA", argc=4)
+    @apihook("GetModuleBaseNameW", argc=4)
+    def GetModuleBaseName(self, emu, argv, ctx={}):
+        hProcess, hModule, lpBaseName, nSize = argv
+        if not lpBaseName or nSize == 0:
+            return 0
+
+        proc = self.get_object_from_handle(hProcess)
+        if not proc:
+            return 0
+
+        module_name = self._get_module_base_name(proc, hModule)
+        if not module_name:
+            return 0
+
+        try:
+            cw = self.get_char_width(ctx)
+        except Exception:
+            cw = 1
+
+        truncated = module_name[: max(nSize - 1, 0)]
+        if cw == 1:
+            output = truncated.encode("utf-8") + b"\x00"
+        else:
+            output = truncated.encode("utf-16le") + b"\x00\x00"
+
+        self.mem_write(lpBaseName, output)
+        return len(truncated)
